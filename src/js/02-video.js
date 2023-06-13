8b44792f02516c29e1f566660e8e533365c56980
Marjan Bat'kivs'kiy <igorovych@gmail.com>
2023-06-13 23:44:54 +0300

    task_1 and task_3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,45 @@
+import Player from '@vimeo/player';
+
+const KEY_VIDEOTIME = 'videoplayer-current-time';
+const player = new Player('vimeo-player', {
+    id: 19231868,
+    width: 640
+});
+
+const onPlay = function({seconds}) {
+    // data is an object containing properties specific to that event
+    
+   localStorage.setItem(KEY_VIDEOTIME, JSON.stringify(seconds));
+//     player.getCurrentTime().then(function(seconds) {
+//     // seconds = the current playback position
+//         // console.log(currentTime)
+//         localStorage.setItem(KEY_VIDEOTIME, JSON.stringify(seconds));
+// }).catch(function(error) {
+//     // an error occurred
+// });
+    const time = JSON.parse(localStorage.getItem(KEY_VIDEOTIME));
+    player.setCurrentTime(time).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+        
+        console.log(time);
+
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+};
+player.on('play', onPlay);
+
+
+
+
+// console.log(player);
+// player.off('play', onPlay);
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle'
 
+const feedbackForm = document.querySelector('.feedback-form');
+const inputForm = document.querySelector('input');
+const textareaForm = document.querySelector('textarea');
+
+const formData = {};
+
+const KEYFORM = 'feedback-form-state';
+const saveFormData = JSON.parse(localStorage.getItem(KEYFORM));
+
+populateForm();
+
+feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+feedbackForm.addEventListener('submit', onFormSubmit);
+
+function onFormInput(evt) {
+
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(KEYFORM, JSON.stringify(formData));
+
+};
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(saveFormData);
+    evt.currentTarget.reset();
+    localStorage.removeItem(KEYFORM);
+};
+
+function populateForm() {
+  
+    if (saveFormData) {
+       
+        inputForm.value = saveFormData.email;
+        textareaForm.value = saveFormData.message;
+    };
+};
