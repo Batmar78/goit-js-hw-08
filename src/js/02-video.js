d279f9d3d413929fcbd991713d81465a1ef9adc1
Marjan Bat'kivs'kiy <igorovych@gmail.com>
2023-06-15 21:56:21 +0300

    JS-HW-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,45 +1,27 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
 const KEY_VIDEOTIME = 'videoplayer-current-time';
+const currentTime = JSON.parse(localStorage.getItem(KEY_VIDEOTIME));
 const player = new Player('vimeo-player', {
     id: 19231868,
     width: 640
 });
 
-const onPlay = function({seconds}) {
-    // data is an object containing properties specific to that event
-    
-   localStorage.setItem(KEY_VIDEOTIME, JSON.stringify(seconds));
-//     player.getCurrentTime().then(function(seconds) {
-//     // seconds = the current playback position
-//         // console.log(currentTime)
-//         localStorage.setItem(KEY_VIDEOTIME, JSON.stringify(seconds));
-// }).catch(function(error) {
-//     // an error occurred
-// });
-    const time = JSON.parse(localStorage.getItem(KEY_VIDEOTIME));
-    player.setCurrentTime(time).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-        
-        console.log(time);
+player.on('timeupdate', throttle(onUpdateTime, 1000));
 
+function onUpdateTime({ seconds }) {
+    localStorage.setItem(KEY_VIDEOTIME, JSON.stringify(seconds))
+};
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+    
 }).catch(function(error) {
     switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
+        case 'RangeError':    
             break;
-
-        default:
-            // some other error occurred
+        default:  
             break;
     }
 });
-};
-player.on('play', onPlay);
-
-
-
-
-// console.log(player);
-// player.off('play', onPlay);
 
