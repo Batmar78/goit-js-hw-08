f52f3f73b6131df5ddca62cf5e9d890c73a1e40d
Marjan Bat'kivs'kiy <igorovych@gmail.com>
2023-06-19 00:36:45 +0300

    HW08JS_v4

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,13 +4,12 @@ const feedbackForm = document.querySelector('.feedback-form');
 const inputForm = document.querySelector('input');
 const textareaForm = document.querySelector('textarea');
 
-const formData = {};
-
 const KEYFORM = 'feedback-form-state';
-
-const saveFormData = JSON.parse(localStorage.getItem(KEYFORM)) ?? formData;
-
-
+const saveFormData = JSON.parse(localStorage.getItem(KEYFORM));
+const formData = {
+    email: '',
+    message: '',
+};
 
 feedbackForm.addEventListener('input', throttle(onFormInput, 500));
 feedbackForm.addEventListener('submit', onFormSubmit);
@@ -18,7 +17,7 @@ feedbackForm.addEventListener('submit', onFormSubmit);
 populateForm();
 
 function onFormInput(evt) {
-   
+
     formData[evt.target.name] = evt.target.value;
     localStorage.setItem(KEYFORM, JSON.stringify(formData));
 
@@ -27,7 +26,7 @@ function onFormInput(evt) {
 function onFormSubmit(evt) {
    
     evt.preventDefault();
-    console.log(formData);
+    console.log(JSON.parse(localStorage.getItem(KEYFORM)));
     evt.target.reset();
     localStorage.removeItem(KEYFORM);
     formData.email = '';
@@ -36,11 +35,12 @@ function onFormSubmit(evt) {
 };
 
 function populateForm() {
-  
-    if (saveFormData) {
-       
+    
+    if (saveFormData) {       
         inputForm.value = saveFormData.email ?? '';
         textareaForm.value = saveFormData.message ?? '';
-       
+        formData.email = inputForm.value ?? '';
+        formData.message = textareaForm.value ?? '';
+    console.log('Formdata', formData);
     };
 };
